4f5bf51e1a18c125118754977d34d13037daa880
sujahathmsm <sujahathmhmd@gmail.com>
2024-05-19 22:19:46 +0530

    Added Customer DTO and Controller

---
+++ src/pos/layered/controller/CustomerController.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pos.layered.controller;
+
+import pos.layered.dto.CustomerDTO;
+
+/**
+ *
+ * @author sujah
+ */
+public class CustomerController {
+
+    public String saveCustomer(CustomerDTO customerDTO) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
